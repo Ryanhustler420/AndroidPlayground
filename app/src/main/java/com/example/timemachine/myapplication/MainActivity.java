b3857fb79baa1d20f25a4bb0ab85b3f5b2fa9237
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-19 22:13:38 +0530

    Activity LifeCycle  Live Explanation of Lifecycle through LogCat

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -1,20 +1,56 @@
 package com.example.timemachine.myapplication;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "OnDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "OnPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "OnResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "OnRestart");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "OnCreate");
     }
 
     public void performAction(View view) {
