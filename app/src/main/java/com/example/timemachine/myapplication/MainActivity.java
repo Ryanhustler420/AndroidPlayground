2a99600484d8e12d03f03b0f71e0e58e5bbffd3d
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-19 20:39:20 +0530

    Distinguish between two buttons

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -9,20 +9,30 @@ import android.widget.Button;
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private Button button;
+    private Button index, marketplace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(MainActivity.this);
+        index = findViewById(R.id.index);
+        index.setOnClickListener(MainActivity.this);
+
+        marketplace = findViewById(R.id.marketPlace);
+        marketplace.setOnClickListener(MainActivity.this);
     }
 
     @Override
     public void onClick(View view) {
-        Log.d(TAG, " First Button Clicked!");
+        switch (view.getId()){
+            case R.id.index:
+                Log.d(TAG, " Index Search Page Clicked");
+                break;
+            case R.id.marketPlace:
+                Log.d(TAG, " Marketplace Intent");
+                break;
+        }
     }
 
 }
