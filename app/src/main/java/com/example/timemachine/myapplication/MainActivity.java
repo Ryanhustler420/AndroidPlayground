2e4d9342e937e3f22abb0a22bb2ba505bb143c5b
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-19 20:27:21 +0530

    FirstButton Implemented

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.timemachine.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(MainActivity.this);
     }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, " First Button Clicked!");
+    }
+
 }
