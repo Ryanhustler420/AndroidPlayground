54280986b7985d79beb5125fd681a1ec26e50d18
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-20 07:52:37 +0530

    Custom Toast and alignment - My Image As Toast

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.timemachine.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,70 +13,19 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "OnStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "OnStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "OnDestroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "OnPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "OnResume");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(TAG, "OnRestart");
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "OnCreate");
     }
 
     public void performAction(View view) {
-        switch (view.getId()){
-            case R.id.m1:
-                    Log.i(TAG, "First Method Worked");
-                    Intent i = new Intent(MainActivity.this, Main2Activity.class);
-                    startActivity(i);
-                    Toast toast = Toast.makeText(MainActivity.this, "Moving to Main2Activity", Toast.LENGTH_SHORT);
-                    toast.setGravity(Gravity.CENTER,0,0);
-                    // toast.setGravity(Gravity.TOP | Gravity.RIGHT,0,0);
-                    toast.show();
-                break;
-            case R.id.m2:
-                    Log.i(TAG, "Second Method Worked");
-                    Intent j = new Intent("Main2Activity");
-                    startActivity(j);
-                break;
-            case R.id.m3:
-                    Log.i(TAG, "Third Method Worked");
-                    Intent k = new Intent();
-                    k.setClassName("com.example.timemachine.myapplication","com.example.timemachine.myapplication.Main2Activity");
-                    startActivity(k);
-                break;
-        }
+        LayoutInflater inflater = getLayoutInflater();
+        View appearance = inflater.inflate(R.layout.custom_layout, (ViewGroup) findViewById(R.id.root_layout));
+        Toast toast = new Toast(MainActivity.this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+
+        toast.setView(appearance);
+        toast.show();
     }
 }
