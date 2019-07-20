7bf908c5095f633128645fbcc786b767e0219312
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-20 08:10:01 +0530

    Fix Method Toast

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.timemachine.myapplication;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -25,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
 
         if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            Toast.makeText(getApplicationContext(), "Portrait Mode", Toast.LENGTH_SHORT);
+            Log.i(TAG,"Portrait");
+            Toast.makeText(getApplicationContext(), "Portrait Mode", Toast.LENGTH_SHORT).show();
         } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(getApplicationContext(), "Landscape Mode", Toast.LENGTH_SHORT);
+            Log.i(TAG,"Landscape");
+            Toast.makeText(getApplicationContext(), "Landscape Mode", Toast.LENGTH_SHORT).show();
         }
     }
 }
