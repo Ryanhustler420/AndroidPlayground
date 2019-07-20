1404a8623bcd4b99840e308bde2f930179304847
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-20 08:39:21 +0530

    Preserving values by - onSaveInstanceState() , onRestoreInstanceState()
    normally the state saves by the system but be aware it could be null. so save those values manually

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -2,21 +2,44 @@ package com.example.timemachine.myapplication;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     int score;
+    EditText et;
+    String etString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         score = 0;
+        et = findViewById(R.id.editText);
+    }
+
+    // called when orientation changes to save the value. when activity is about to die || destroy
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        etString = et.getText().toString();
+        outState.putString("edittext", etString);
+        outState.putInt("score", score);
+    }
+
+    // called when this activity again run lifeCycle from start aka OnCreate, we will get our saved value from here.
+    // we will extract the values from Bundle
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        et.setText(savedInstanceState.getString("edittext"));
+        score = savedInstanceState.getInt("score");
     }
 
     public void performAction(View view) {
