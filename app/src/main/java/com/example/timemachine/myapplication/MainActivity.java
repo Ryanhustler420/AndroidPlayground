4d82048575df014c3e21a866d541c1e270d5cdc8
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-20 08:01:38 +0530

    Screen Orientation Manual - BAD PRACTICE
    enter the commented out code inside activity tag

--- app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
+++ app/src/main/java/com/example/timemachine/myapplication/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.timemachine.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +15,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void performAction(View view) {
-        LayoutInflater inflater = getLayoutInflater();
-        View appearance = inflater.inflate(R.layout.custom_layout, (ViewGroup) findViewById(R.id.root_layout));
-        Toast toast = new Toast(MainActivity.this);
-        toast.setDuration(Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.CENTER, 0, 0);
 
-        toast.setView(appearance);
-        toast.show();
     }
 }
