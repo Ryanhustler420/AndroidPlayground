e835701235bfed1386f7335ab8bbf824d92f0ed7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-21 12:20:53 +0530

    Created simple and tough questions activities

--- app/src/main/java/com/example/timemachine/myapplication/FrontPage.java
+++ app/src/main/java/com/example/timemachine/myapplication/FrontPage.java
@@ -1,13 +1,45 @@
 package com.example.timemachine.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class FrontPage extends AppCompatActivity {
+public class FrontPage extends AppCompatActivity implements View.OnClickListener {
+
+    Button bSimple, bTough, bcoa, brate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_page);
+        bSimple = findViewById(R.id.bsq);
+        bTough = findViewById(R.id.btq);
+        bcoa = findViewById(R.id.coa);
+        brate = findViewById(R.id.rate);
+
+        bSimple.setOnClickListener(this);
+        bTough.setOnClickListener(this);
+        bcoa.setOnClickListener(this);
+        brate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bsq:
+                Intent simple = new Intent(FrontPage.this, Simple_question.class);
+                startActivity(simple);
+                break;
+            case R.id.btq:
+                Intent tough = new Intent(FrontPage.this, Tough_question.class);
+                startActivity(tough);
+                break;
+            case R.id.coa:
+                break;
+            case R.id.rate:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/timemachine/myapplication/Tough_question.java
@@ -0,0 +1,13 @@
+package com.example.timemachine.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tough_question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tough_question);
+    }
+}
