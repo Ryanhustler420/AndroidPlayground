cd4d60b5a59fba46e834c337433521d1756b24d9
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-21 22:12:58 +0530

    Variable and Widgets Initialization in Java Code

--- app/src/main/java/com/example/timemachine/myapplication/QuestionAnswerStore.java
+++ app/src/main/java/com/example/timemachine/myapplication/QuestionAnswerStore.java
@@ -1,25 +1,21 @@
 package com.example.timemachine.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Simple_question extends AppCompatActivity implements View.OnClickListener {
+import java.util.Arrays;
 
+public class QuestionAnswerStore extends AppCompatActivity implements View.OnClickListener {
     TextView tvQuestion, tvAnswer, tvTotalLength__yy, tvPresentIndex_xx;
+    String[] allQuestions, allAnswers;
 
     Button bprev, bshow, bnext;
 
     int index;
-    String[] simpleQuestions, simpleAnswers;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.questions);
-
+    public QuestionAnswerStore QuestionAnswerStore(String[] questions, String[] answers) {
         tvQuestion = findViewById(R.id.tvquestion);
         tvAnswer = findViewById(R.id.tvanswer);
         tvPresentIndex_xx = findViewById(R.id.tvxx);
@@ -33,16 +29,19 @@ public class Simple_question extends AppCompatActivity implements View.OnClickLi
         bshow.setOnClickListener(this);
         bnext.setOnClickListener(this);
 
+        allQuestions = questions.clone();
+        allAnswers = Arrays.copyOf(answers, answers.length);
+
         // init first question
-        tvQuestion.setText(simpleQuestions[index]);
+        tvQuestion.setText(allQuestions[index]);
         tvAnswer.setText("Press \"A\" Button for the Answer");
         tvPresentIndex_xx.setText(String.valueOf(index+1));
-        tvTotalLength__yy.setText(String.valueOf(simpleQuestions.length));
+        tvTotalLength__yy.setText(String.valueOf(allQuestions.length));
         index = 0;
 
-        simpleQuestions = getResources().getStringArray(R.array.simple_Questions);
-        simpleAnswers = getResources().getStringArray(R.array.simple_answers);
+        return this;
     }
+
 
     @Override
     public void onClick(View view) {
@@ -58,4 +57,6 @@ public class Simple_question extends AppCompatActivity implements View.OnClickLi
                 break;
         }
     }
+
+    // methods for incress decres
 }
--- app/src/main/java/com/example/timemachine/myapplication/Tough_question.java
+++ app/src/main/java/com/example/timemachine/myapplication/Tough_question.java
@@ -2,8 +2,15 @@ package com.example.timemachine.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Tough_question extends AppCompatActivity {
+public class Tough_question extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvQuestion, tvAnswer, tvTotalLength__yy, tvPresentIndex_xx;
+
+    Button bprev, bshow, bnext;
 
     String[] toughQuestions, toughAnswers;
 
@@ -11,7 +18,36 @@ public class Tough_question extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
+
+        tvQuestion = findViewById(R.id.tvquestion);
+        tvAnswer = findViewById(R.id.tvanswer);
+        tvPresentIndex_xx = findViewById(R.id.tvxx);
+        tvTotalLength__yy = findViewById(R.id.tvyy);
+
+        bprev = findViewById(R.id.bprev);
+        bshow = findViewById(R.id.bshowanswer);
+        bnext = findViewById(R.id.bnext);
+
+        bprev.setOnClickListener(this);
+        bshow.setOnClickListener(this);
+        bnext.setOnClickListener(this);
+
         toughQuestions = getResources().getStringArray(R.array.tough_questions);
         toughAnswers = getResources().getStringArray(R.array.tough_answers);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bprev:
+
+                break;
+            case R.id.bshowanswer:
+
+                break;
+            case R.id.bnext:
+
+                break;
+        }
     }
 }
