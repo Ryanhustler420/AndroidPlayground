439b59828455d45d0f32a001bb61021be7cbeca0
Gaurav Gupta <gouravgupta840@gmail.com>
2019-07-21 20:10:53 +0530

    Strings.xml  Added Questtions and Answers. Respective Category
    Easy and Tough.!

--- app/src/main/java/com/example/timemachine/myapplication/Simple_question.java
+++ app/src/main/java/com/example/timemachine/myapplication/Simple_question.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class Simple_question extends AppCompatActivity {
 
+    String[] simpleQuestions, simpleAnswers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simple_question);
+        setContentView(R.layout.questions);
+        simpleQuestions = getResources().getStringArray(R.array.simple_Questions);
+        simpleAnswers = getResources().getStringArray(R.array.simple_answers);
     }
 }
--- app/src/main/java/com/example/timemachine/myapplication/Tough_question.java
+++ app/src/main/java/com/example/timemachine/myapplication/Tough_question.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class Tough_question extends AppCompatActivity {
 
+    String[] toughQuestions, toughAnswers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tough_question);
+        setContentView(R.layout.questions);
+        toughQuestions = getResources().getStringArray(R.array.tough_questions);
+        toughAnswers = getResources().getStringArray(R.array.tough_answers);
     }
 }
